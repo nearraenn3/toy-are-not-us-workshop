2e5e9aba82f91a91c67f9dbeabc1969ced25fdf7
Numlabyod Reprakan <numlabyodrep@mathregit.group>
2020-07-31 00:32:34 +0700

    fix id

--- frontend/toy-r-not-us/src/app/toy-product/toy-product.component.ts
+++ frontend/toy-r-not-us/src/app/toy-product/toy-product.component.ts
@@ -14,10 +14,10 @@ export class ToyProductComponent implements OnInit {
   constructor(public service: ProductService) { }
 
   ngOnInit(): void {
-    const p1 = new Product ( 11,'Sleep Sheep','Neutral','baby', 39 , 'Instock', 'BarnyyardBlast')
-    const p2 = new Product ( 12,'Scrabble','Neutral','over8', 19.95 , 'Instock', 'Graeektoy')
-    const p3 = new Product ( 13,'AA','male','over8', 19 , 'Instock', 'ddd')
-    const p4 = new Product ( 14 ,'BB','female','3to5', 24 , 'Instock', 'ccc')
+    const p1 = new Product ( 11,'Sleep Sheep','Neutral','baby', 39 , 'In Stock', 'BarnyyardBlast')
+    const p2 = new Product ( 12,'Scrabble','Neutral','over8', 19.95 , 'In Stock', 'GeekToys')
+    const p3 = new Product ( 13,'AA','male','over8', 19 , 'In Stock', 'ddd')
+    const p4 = new Product ( 14 ,'BB','female','3to5', 24 , 'In Stock', 'ccc')
       
     this.products.push(p1,p2,p3,p4);
   }
