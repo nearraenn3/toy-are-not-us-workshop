13670c3863ddf293d3ab2e5ab198fc0dfe0d1f40
Hasaneeya Kaenthram <hasaneeyakae@mathregit.group>
2020-07-30 15:32:37 +0700

    create productdetailComponent and update routes path

--- frontend/toy-r-not-us/src/app/app-routing.module.ts
+++ frontend/toy-r-not-us/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //Import Component
 import { ToyProductComponent } from './toy-product/toy-product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { from } from 'rxjs';
 
 //Create component routes path
 const routes: Routes = [
   { path: 'toy-product', component: ToyProductComponent},
-
+  { path: 'toy-product', component: ProductDetailComponent},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
