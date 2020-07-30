d7e7a27e5c2d17b81404cacce9c3c0d7b96c18f3
Hasaneeya Kaenthram <hasaneeyakae@mathregit.group>
2020-07-30 15:27:07 +0700

    updatecode: app.component.html

--- frontend/toy-r-not-us/src/app/app.module.ts
+++ frontend/toy-r-not-us/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToyProductComponent } from './toy-product/toy-product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToyProductComponent
+    ToyProductComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
