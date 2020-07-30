cfa8b0cea9178ca1df9ffddd29521421f4b3cf63
Numlabyod Reprakan <numlabyodrep@mathregit.group>
2020-07-30 15:30:39 +0700

    update file app

--- frontend/toy-r-not-us/src/app/app-routing.module.ts
+++ frontend/toy-r-not-us/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToyProductComponent } from './toy-product/toy-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'toy-product', component: ToyProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
