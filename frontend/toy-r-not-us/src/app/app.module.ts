ebd4978756e5b555aa2082e6b36f0fe75fc24d2e
Hasaneeya Kaenthram <hasaneeyakae@mathregit.group>
2020-07-30 18:08:00 +0700

    create cart component

--- frontend/toy-r-not-us/src/app/app.module.ts
+++ frontend/toy-r-not-us/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ToyProductComponent } from './toy-product/toy-product.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { MainComponent } from './main/main.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToyProductComponent,
     ProductDetailComponent,
-    MainComponent
+    MainComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
