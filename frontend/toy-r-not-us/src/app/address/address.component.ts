ce58e3579c997cf0a639e9a1e933e3966326a321
Hasaneeya Kaenthram <hasaneeyakae@mathregit.group>
2020-07-30 19:42:10 +0700

    update code : valid required ; address page

--- frontend/toy-r-not-us/src/app/address/address.component.ts
+++ frontend/toy-r-not-us/src/app/address/address.component.ts
@@ -9,7 +9,26 @@ export class AddressComponent implements OnInit {
 
   constructor() { }
 
+  
   ngOnInit(): void {
+    //Check Valid forms 
+
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+      var forms = document.getElementsByClassName('needs-validation');
+      var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+      }, false);
+      });
+      }, false);
+      })();
   }
+  
 
 }
